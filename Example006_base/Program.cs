dff84b16479108cc9ba1643734e2578312c22da0
Vasil <vasilgm@yandex.ru>
2023-03-22 19:06:05 +0300

    examle006_base

---
+++ Example006_base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
