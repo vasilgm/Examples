371379e130289ef9085e2b6b8adbcd8cda5d0341
Vasil <vasilgm@yandex.ru>
2023-03-23 21:25:49 +0300

    example008 and 009

---
+++ Example008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2:");
+int number2 = int.Parse(Console.ReadLine());
+
+if ( number2 * number2 == number1)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
---
+++ Example009/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите номер дня:");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+Console.WriteLine("Понедельник");
+}
+
+if (number == 2)
+{
+Console.WriteLine("Вторник");
+}
+
+if (number == 3)
+{
+Console.WriteLine("Среда");
+}
+
+if (number == 4)
+{
+Console.WriteLine("Четверг");
+}
+
+if (number == 5)
+{
+Console.WriteLine("Пятница");
+}
+
+if (number == 6)
+{
+Console.WriteLine("Суббота");
+}
+
+if (number == 7)
+{
+Console.WriteLine("Воскресенье");
+}
+else
+{
+Console.WriteLine("Нет такого дня!");
+}
