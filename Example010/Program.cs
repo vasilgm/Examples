060a7b4c6b11c1eaebe70d9c4bfc53ceaa33cfda
Vasil <vasilgm@yandex.ru>
2023-03-23 22:29:00 +0300

    example010 and 011

---
+++ Example010/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = - number; i <= number; i++)
+{
+    Console.Write(i);
+    Console.Write(",");
+}
---
+++ Example011/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int a = int.Parse(Console.ReadLine());
+
+int b = a%10;
+
+Console.WriteLine(b);
