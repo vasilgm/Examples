655d217dcec454d447f4b5b571b99641685a8956
Vasil <vasilgm@yandex.ru>
2023-03-26 14:25:30 +0300

    example014

---
+++ example014/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11, 22, 30, 4, 55, 61, 72, 83, 4};
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (find == array[index])
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
