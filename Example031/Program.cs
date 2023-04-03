abc9292d6ba65e5f1bf3d748af3fb86faa00f2ff
Vasil <vasilgm@yandex.ru>
2023-04-03 22:22:53 +0300

    031

---
+++ Example031/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.WriteLine(array[i]);
+}
+
